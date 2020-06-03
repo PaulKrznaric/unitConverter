5178955dc8a0fa0262b36247ac855d28d6599f00
Paul Krznaric <paulkrznaric@gmail.com>
2020-06-03 00:28:25 -0400

    Initial Commit

---
+++ unitConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  unitConverter
+//
+//  Created by Paul Krznaric on 2020-06-03.
+//  Copyright © 2020 Krznarnetic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
